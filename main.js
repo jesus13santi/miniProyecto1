dd7142367260a922d8b3240141d852f50bef3c1c
Jesus <jesus13santi@gmail.com>
2021-10-17 21:50:51 -0400

    Funcionalidades javaScript

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var imagenes = ["img/keyboard.jpg", "img/lapto.jpg", "img/macbook.jpg"],
   cont = 0;
 let img = document.getElementById("fondo");
+const btnSend = document.getElementById("btnEnviar");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("textarea");
+const mainSectionContainer = document.getElementById("skills");
 
 window.addEventListener("DOMContentLoaded", function () {
   console.log("El contenido se ha cargado");
@@ -17,7 +22,59 @@ window.addEventListener("DOMContentLoaded", function () {
   setInterval(cambiarImagenes, 10000);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  let contenedor = document.querySelector(".box-home");
-  carrousel(img);
+let newNombre = "";
+let newEmail = "";
+let newMensaje = "";
+
+nombre.addEventListener("change", (event) => {
+  newNombre = event.target.value;
 });
+email.addEventListener("change", (event) => {
+  newEmail = event.target.value;
+});
+mensaje.addEventListener("change", (event) => {
+  newMensaje = event.target.value;
+});
+
+btnSend.addEventListener("click", () => {
+  if (newNombre == "" || newEmail == "" || newMensaje == "") {
+    alert("Error al enviar el formulario");
+  } else {
+    console.log(`
+    Nombre: ${newNombre}
+    Email: ${newEmail}
+    Mensaje: ${newMensaje}
+    `);
+    alert("Su solicitud ha sido enviada");
+  }
+});
+const skills = [
+  {
+    Nombre: "Java",
+    Porcentaje: 50,
+  },
+  {
+    Nombre: "Phyton",
+    Porcentaje: 80,
+  },
+  {
+    Nombre: "HTML",
+    Porcentaje: 70,
+  },
+  {
+    Nombre: "CSS",
+    Porcentaje: 80,
+  },
+  {
+    Nombre: "JavaScript",
+    Porcentaje: 10,
+  },
+  {
+    Nombre: "Git",
+    Porcentaje: 60,
+  },
+  {
+    Nombre: "GitHub",
+    Porcentaje: 50,
+  },
+];
